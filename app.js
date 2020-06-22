4551d8c040fc08bd85222c5c5cbcc97d117b989c
RedwanPlague <mdredwan.rh@gmail.com>
2020-06-22 20:35:25 +0600

    SDN for app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-console.log("this is me")
-console.log('hello')
-
-console.log("ami red")
-
-console.log("farm")
+console.log("this is me");
+console.log('hello');
+console.log("ami red");
+console.log("farm");
+console.log("SDN er prothom ta");
