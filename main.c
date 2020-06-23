040422b65b0310633b014b086b8b23b797f95c2c
RedwanPlague <mdredwan.rh@gmail.com>
2020-06-23 12:06:11 +0600

    edited main.c, added gitignore, makefile

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main() {
     int a;
     scanf("%d", &a);
-    printf("%d is not only -(-%d), but also 1/(1/%d))", a, a, a);
+    printf("%d is not only -(-%d), but also 1/(1/%d))\n", a, a, a);
     return 0;
 }
